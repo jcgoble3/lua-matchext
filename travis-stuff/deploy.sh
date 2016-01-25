728998830e260244f7ad2b7506dea5492150b40a
Jonathan Goble <jcgoble3@gmail.com>
2016-01-24 23:47:39 -0500

    tweak deployment to hopefully make log cleaner

--- travis-stuff/deploy.sh
+++ travis-stuff/deploy.sh
@@ -4,7 +4,4 @@ set -ev
 
 sed -i "s/##__TAG__##/$TRAVIS_TAG/" "$ROCKSPEC"
 
-# upload requires a JSON library
-luarocks install lua-cjson
-
 luarocks upload --api-key="$LUAROCKS_API_KEY" "$ROCKSPEC"
