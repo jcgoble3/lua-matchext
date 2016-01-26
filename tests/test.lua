483bf8509156a46b64fa0af07ce297dc67564783
Jonathan Goble <jcgoble3@gmail.com>
2016-01-26 00:05:38 -0500

    convert test suite to busted

--- tests/test.lua
+++ tests/test.lua
@@ -1,15 +1,10 @@
 #!/usr/bin/env lua
 
+require 'busted.runner'()
+
 versionnum = tonumber(_VERSION:match('%d%.%d'))
 
 matchext = require 'matchext'
-
--- Make sure all functions are present
-assert(matchext.find)
-assert(matchext.match)
-assert(matchext.gmatch)
-assert(matchext.gsub)
-assert(matchext.monkeypatch)
 
 -- Centralized listing of values for tests, for easy re-use and maintenance
 vals = {
@@ -20,34 +15,54 @@ vals = {
     }
 }
 
--- Test %B balancing with escape character
-assert(matchext.match(vals.B.str, vals.B.patt) == vals.B.result)
--- Stock match should throw error for unfinished capture group
-assert(not pcall(string.match, vals.B.str, vals.B.patt))
+describe('All tests:', function()
+    it('Make sure functions are present', function()
+        assert.is_function(matchext.find)
+        assert.is_function(matchext.match)
+        assert.is_function(matchext.gmatch)
+        assert.is_function(matchext.gsub)
+        assert.is_function(matchext.monkeypatch)
+    end)
 
+    it('Test %B balancing with escape character', function()
+        assert.equal(matchext.match(vals.B.str, vals.B.patt), vals.B.result)
+    end)
 
--- This is where the fun begins :)
-matchext.monkeypatch()
+    it('Stock match should throw error for unfinished capture', function()
+        assert.error(function() vals.B.str:match(vals.B.patt) end)
+    end)
 
--- Assert that stock functions are present in "original" sub-table
-assert(string.original)
-assert(string.original.find)
-assert(string.original.match)
-assert(string.original.gmatch)
-assert(string.original.gsub)
+    it('Monkey-patch string module', function()
+        assert.has_no.errors(matchext.monkeypatch)
+    end)
 
--- Test that functions were properly copied
--- For some reason, Lua 5.1 makes copies of the functions rather than
--- pointing to the same instance, so run this only on 5.2 and higher
-if versionnum >= 5.2 then
-    assert(string.find == matchext.find)
-    assert(string.match == matchext.match)
-    assert(string.gmatch == matchext.gmatch)
-    assert(string.gsub == matchext.gsub)
-end
+    it('Stock functions present in "original" sub-table', function()
+        assert.is_table(string.original)
+        assert.is_function(string.original.find)
+        assert.is_function(string.original.match)
+        assert.is_function(string.original.gmatch)
+        assert.is_function(string.original.gsub)
+    end)
 
--- Ensure that string.match now handles new features...
-assert(vals.B.str:match(vals.B.patt) == vals.B.result)
+    -- For some reason, Lua 5.1 makes copies of the functions rather than
+    -- pointing to the same instance, so run this only on 5.2 and higher
+    it('Test that functions were properly copied', function()
+        if versionnum >= 5.2 then
+            assert.equal(string.find, matchext.find)
+            assert.equal(string.match, matchext.match)
+            assert.equal(string.gmatch, matchext.gmatch)
+            assert.equal(string.gsub, matchext.gsub)
+        end
+    end)
 
--- ...and that functions in string.original do not
-assert(not pcall(string.original.match, vals.B.str, vals.B.patt))
+    it('String.match should now handles new features', function()
+        assert.has_no.errors(function() vals.B.str:match(vals.B.patt) end)
+        assert.equal(vals.B.str:match(vals.B.patt), vals.B.result)
+    end)
+
+    it('Functions in string.original should still error', function()
+        assert.error(function()
+            string.original.match(vals.B.str, vals.B.patt)
+        end)
+    end)
+end)
